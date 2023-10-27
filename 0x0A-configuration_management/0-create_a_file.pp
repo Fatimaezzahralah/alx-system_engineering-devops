5aa045a200e92ff136e81e4ed54bbd52bcfad6a8
Fatimaezzahralah <fatimaezzahra288@gmail.com>
2023-10-27 07:50:37 -0700

    tasks

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# Create a file using puppet
+#
+file { '/tmp/school':
+  ensure  => file,
+  path    => '/tmp/school',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet'
+}
